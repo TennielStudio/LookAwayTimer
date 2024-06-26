0048817313ac8d9ff754465b828fa88bbd240aeb
Tenniel <tenniel.lynn@gmail.com>
2024-06-26 06:58:34 -0700

    first commit, get basic timer functionality done

---
+++ README.md
@@ -0,0 +1,5 @@
+# LookAwayTimer
+
+A fun timer that reduces digital eye strain. It encourages you to look away from the screen at regular intervals.
+
+Made by [TennielStudio](https://twitter.com/tennielstudio) ❤️
---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+import { useTimer } from 'react-timer-hook'
+
+// Constants
+const SECONDS_TO_JUMP = 1200 
+
+function MyTimer ({ expiryTimestamp }) {
+  const [isPaused, setIsPaused] = useState(false)
+
+  const {
+    seconds,
+    minutes,
+    isRunning,
+    start,
+    pause,
+    resume,
+    restart,
+  } = useTimer({ autoStart: false, expiryTimestamp, onExpire: () => console.warn('onExpire called') });
+
+  const handlePause = () => {
+    pause()
+    setIsPaused(true)
+  }
+
+  const handleResume = () => {
+    resume()
+    setIsPaused(false)
+  }
+
+  const handleStart = () => {
+    start()
+    setIsPaused(false)
+  }
+
+  return (
+    <div style={{textAlign: 'center'}}>
+      <h1>react-timer-hook </h1>
+      <p>Timer Demo</p>
+      <div style={{fontSize: '100px'}}>
+        <span>{minutes}</span>:<span>{seconds.toString().padStart(2,'0')}</span>
+      </div>
+      
+      {!isRunning && !isPaused && <button onClick={handleStart}>Start</button>}
+      {isRunning && <button onClick={handlePause}>Pause</button>}
+      {isPaused && <button onClick={handleResume}>Resume</button>}
+
+      <button onClick={() => {
+        // Restarts timer
+        const time = new Date();
+        time.setSeconds(time.getSeconds() + SECONDS_TO_JUMP);
+        restart(time, false)
+      }}>Restart</button>
+    </div>
+  );
+}
+
+function App() {
+  const time = new Date(); // anchor ourselves in current time
+  time.setSeconds(time.getSeconds() + SECONDS_TO_JUMP); // 20 minutes timer <- this updates the date object to exactly 20 mins in the future
+  return (
+    <div>
+      <MyTimer expiryTimestamp={time} />
+    </div>
+  );
+}
+
+export default App
