18874a7cbdca69f59f260fad6ee54c95f661deb3
Tenniel <tenniel.lynn@gmail.com>
2024-07-22 06:59:24 -0700

    added css designs

--- src/components/MyTimer.jsx
+++ src/components/MyTimer.jsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from 'react'
 import { useTimer } from 'react-timer-hook'
-import DuckLaptop from "../assets/duck-laptop.png"
+import RealDuck from "../assets/real-duck.webp"
+import Banner from "../assets/banner.png"
 import TimerButtons from "./TimerButtons"
+import TextBubble from "../assets/text-bubble.png"
+import Laptop from "../assets/laptop.webp"
+import Toast from "../assets/toast.webp"
 import { TWENTY_MINUTES_IN_SEC, TWENTY_SECONDS } from '../TimerConstants'
 
 const TimerExpired = ({ timerDuration, setNextTimeInterval, setHasExpired }) => {
@@ -61,25 +65,56 @@ const MyTimer = ({expiryTimestamp, timerDuration}) => {
   }
 
   return (
-    <div style={{textAlign: 'center'}}>
-    <h1>♡ My Cute Timer ♡ </h1>
-      <div className="image-container">
-        <img src={DuckLaptop} alt="" />
+    <div className="full-page" style={{textAlign: 'center'}}>
+    <div className="title-container">
+      <h1 className="title">♡ DuckyVision 3000 ♡ </h1>
+      <div className="banner-container">
+          <img src={Banner} alt="" />
       </div>
-      <p>how the girlies stay on time</p>
-      <div style={{fontSize: '100px'}}>
-        <span>{minutes}</span>:<span>{seconds.toString().padStart(2,'0')}</span>
+    </div>
+
+      <div className="timer">
+        <div className="time" style={{fontSize: '100px'}}> 
+          <span>{minutes}</span>:<span>{seconds.toString().padStart(2,'0')}</span>
+        </div>
+      
+        <div className="timer-buttons">
+          <TimerButtons
+          isRunning={isRunning}
+          isPaused={isPaused}
+          handleStart={handleStart}
+          handlePause={handlePause}
+          handleResume={handleResume}
+          timerDuration={timerDuration}
+          restart={restart}
+          setIsPaused={setIsPaused}/> 
+        </div>
       </div>
 
-      <TimerButtons
-      isRunning={isRunning}
-      isPaused={isPaused}
-      handleStart={handleStart}
-      handlePause={handlePause}
-      handleResume={handleResume}
-      timerDuration={timerDuration}
-      restart={restart}
-      setIsPaused={setIsPaused}/>
+      <div className="lower-page-container">
+        <div className="duck-container">
+          <img className="real-duck" src={RealDuck} alt="" />
+        </div>
+        <div className="text-bubble-content-container">
+            <p className="text-bubble-content">
+              Ducks have better vision than humans!
+              Why? We take regular screen breaks.
+              And we use this timer to do it. Quack.</p>
+          </div>
+        <div className="text-bubble-container">
+          <img className="text-bubble" src={TextBubble} alt=""/>
+        </div>
+        <div className="laptop-container">
+          <img className="laptop" src={Laptop} alt=""/>
+        </div>
+        <div className="toast-container">
+          <img className="toast" src={Toast} alt=""/>
+        </div>
+        <div className="yummy-taas-link-container">
+          <a className="yummy-taas-link" href="https://www.yummytaas.com/" target="_blank" rel="noopener noreferrer"/>
+        </div>
+      </div>
+      <a href="https://www.vecteezy.com/free-vector/pixel">Pixel Vectors by Vecteezy</a>
     </div>
   );
 }
